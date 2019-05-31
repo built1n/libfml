a99daf4a49dfbe9f4ec9bd16da8c2ad274b592a4
Franklin Wei <me@fwei.tk>
2019-05-31 19:08:41 -0400

    Import manifold code from fieldviz

--- include/fml/fml.h
+++ include/fml/fml.h
@@ -4,7 +4,16 @@
  * Copyright (C) 2019 Franklin Wei
  */
 
+#ifndef FML_H
+#define FML_H
+
 typedef float scalar;
 
+#include "curve.h"
+#include "manifold.h"
 #include "quat.h"
+#include "surface.h"
+#include "vec2.h"
 #include "vec3.h"
+
+#endif
